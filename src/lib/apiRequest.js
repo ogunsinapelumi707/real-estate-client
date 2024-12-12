6474c084936d02fbf5911a15ec6df7dcb433c5d7
Pelumi Raphael <pelumiogunsina7@gmail.com>
2024-12-12 17:55:54 +0100

    api endpoint updated

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const apiReuest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    baseURL: apiUrl,
     withCredentials: true
 })
 
