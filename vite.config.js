44d680cd0114ae649107434b165d8e92532b5a9c
Pelumi Raphael <pelumiogunsina7@gmail.com>
2024-12-13 10:15:07 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/"
+  build:{
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'], // Separate common dependencies
+        },
+      },
+    }
+  }
 })
